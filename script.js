265e5f464da838a9f7a3360c9a1fad812b8c3502
oludare89 <azizoyekan@hotmail.com>
2022-12-06 21:55:52 +0000

    Reduced speed of background scrolling

--- script.js
+++ script.js
@@ -85,11 +85,11 @@ window.addEventListener('load', function(){
             this.y = 0;
             this.width = 2400;
             this.height = 720;
-            this.speed = 20;
+            this.speed = 7;
         }
         draw(context){
             context.drawImage(this.image, this.x, this.y, this.width, this.height);
-            context.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+            context.drawImage(this.image, this.x + this.width - this.speed, this.y, this.width, this.height);
         }
         update(){
             this.x -= this.speed;
