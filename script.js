42c93ddaaf5eca766b3bdfe5b3ee3782b7e1652b
oludare89 <azizoyekan@hotmail.com>
2022-12-06 21:45:59 +0000

    Added background constructor and draw function so that background now drawn on canvas

--- script.js
+++ script.js
@@ -77,6 +77,18 @@ window.addEventListener('load', function(){
     }
 
     class Background {
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = document.getElementById('backgroundImage');
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 720;
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
 
     }
 
@@ -94,9 +106,11 @@ window.addEventListener('load', function(){
 
     const input = new InputHandler();
     const player = new Player(canvas.width, canvas.height);
+    const background = new Background(canvas.width, canvas.height);
 
     function animate(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        background.draw(ctx);
         player.draw(ctx);
         player.update(input);
         requestAnimationFrame(animate);
