dc188c7171126d4d68c43fcd1eafd1680c67b988
oludare89 <azizoyekan@hotmail.com>
2022-12-06 21:37:06 +0000

    Added jumping ability with up and down boundaries and can only jump once when on ground

--- script.js
+++ script.js
@@ -39,6 +39,8 @@ window.addEventListener('load', function(){
             this.frameX = 0;
             this.frameY = 0;
             this.speed = 0;
+            this.vy = 0;
+            this.weight = 1;
         }
         draw(context){
             context.fillStyle = 'white';
@@ -46,16 +48,31 @@ window.addEventListener('load', function(){
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
         }
         update(input){
-            this.x += this.speed;
             if (input.keys.indexOf('ArrowRight') > -1){
                 this.speed = 5;
             } else if (input.keys.indexOf('ArrowLeft') > -1){
                 this.speed = -5;
+            } else if (input.keys.indexOf('ArrowUp') > -1 && this.onGround()){
+                this.vy -= 32;
             } else {
                 this.speed = 0;
             }
+
+            // horizontal movement
+            this.x += this.speed;
             if (this.x < 0) this.x = 0;
             else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+            //vertical movement
+            this.y += this.vy;
+            if (!this.onGround()){
+                this.vy += this.weight;
+            } else {
+                this.vy = 0;
+            }
+            if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
+        }
+        onGround(){
+            return this.y >= this.gameHeight - this.height;
         }
     }
 
