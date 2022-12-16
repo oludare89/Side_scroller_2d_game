18dd9286eb6c55c4a1e86c8e3615e5e0e68b8b8a
oludare89 <azizoyekan@hotmail.com>
2022-12-16 10:07:19 +0000

    Added functionality so that when you swipe down when game is over the game restarts

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ window.addEventListener('load', function(){
     class InputHandler {
         constructor(){
             this.keys = [];
+            this.touchY = '';
+            this.touchThreshold = 30;
             window.addEventListener('keydown', e => {
                 if ((   e.key === 'ArrowDown' || 
                         e.key === 'ArrowUp' || 
@@ -26,6 +28,22 @@ window.addEventListener('load', function(){
                         e.key === 'ArrowLeft') {
                     this.keys.splice(this.keys.indexOf(e.key), 1);
                 }
+            });
+            window.addEventListener('touchstart', e => {
+                this.touchY = e.changedTouches[0].pageY;
+            });
+            window.addEventListener('touchmove', e => {
+                const swipeDistance = e.changedTouches[0].pageY - this.touchY;
+                if (swipeDistance < -this.touchThreshold && this.keys.indexOf('swipe up') === -1) this.keys.push('swipe up');
+                else if (swipeDistance > this.touchThreshold && this.keys.indexOf('swipe down') === -1){
+                    this.keys.push('swipe down');
+                    if (gameOver) restartGame();
+                } 
+            });
+            window.addEventListener('touchend', e => {
+                console.log(this.keys);
+                this.keys.splice(this.keys.indexOf('swipe up'), 1);
+                this.keys.splice(this.keys.indexOf('swipe down'), 1);
             });
         }
     }
